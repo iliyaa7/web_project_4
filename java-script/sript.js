619e5a1f18bb2dbbf38f5034c6e3ede73ed6a05b
iliya7 <iliyaa7@gmail.com>
2021-08-23 03:25:23 +0300

    final - correction 1

--- java-script/sript.js
+++ java-script/sript.js
@@ -17,39 +17,22 @@ function openClosePopup() {
 
 
 
-
-
-let formElement = document.querySelector(".popup__container");
+let formElement = document.querySelector("#form");
 let saveButton = document.querySelector(".popup__save-button")
 
 function inputData(evt) {
 evt.preventDefault();
  profileName.textContent = inputName.value;
  profileAbout.textContent = inputAbout.value;
+ popup.classList.remove("popup_opened");
 };
 
 
-
-
-
-
-likeButton = document.querySelector(".post__like-button");
-let srcLength = likeButton.getAttribute("src");
-
-function activeLikeButton() {
-  let srcLength = likeButton.getAttribute("src");
-  if (srcLength.length <= 24) {
-    likeButton.setAttribute("src", "./images/like-button_active.svg");
-  } else {
-    likeButton.setAttribute("src", "./images/like-button.svg");
-  }
-};
 
 
 popupButton.addEventListener("click", openClosePopup);
 closeButton.addEventListener("click", openClosePopup);
-likeButton.addEventListener("click", activeLikeButton);
 formElement.addEventListener("submit", inputData);
-saveButton.addEventListener("click", openClosePopup);
+
 
 
