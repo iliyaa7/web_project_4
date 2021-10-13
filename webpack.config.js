5010f0e3c11b8f8773a3014223a91ffab41f2b31
iliya7 <iliyaa7@gmail.com>
2021-10-13 22:12:19 +0300

    webpack - final stage

--- webpack.config.js
+++ webpack.config.js
@@ -38,8 +38,10 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader"
+            loader: "css-loader",
+            options: { importLoaders: 1 }
           },
+          "postcss-loader"
         ]
       },
       {
